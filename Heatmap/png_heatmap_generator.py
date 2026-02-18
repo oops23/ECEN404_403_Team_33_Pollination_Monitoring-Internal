d69eb7edbaad1096301894fbb3a08e68ddbcd3f3
Paavan Bagla <paavanbagla@gmail.com>
2026-02-18 09:55:12 -0600

    no multiple folder

--- Heatmap/png_heatmap_generator.py
+++ Heatmap/png_heatmap_generator.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from ML.bee_classifier import BeeClassifier
+from bee_classifier import BeeClassifier
 
 # ===============================
 # LIDAR CONFIG
